45901cc46533df6da7a1c48a62aa46f11d38f10a
sbcodes90 <sydneycburrell29@gmail.com>
2024-01-11 18:33:17 -0500

    install tailwind css

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction  } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "../app/tailwind.css"
 
 export const meta: MetaFunction = () => [{
   charset: "utf-8",
   title: "Sydney Burrell Portfolio",
   viewport: "width=device-width,initial-scale=1",
 }];
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
   return (
--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -9,6 +9,7 @@ export function headers({
     "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
   );
   return {
+    
     // This is an example of how to set caching headers for a route
     // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
     "Cache-Control": "public, max-age=60, s-maxage=60",
@@ -19,7 +20,9 @@ export default function Index() {
   return (
     <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-   
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </main>
   );
 }
