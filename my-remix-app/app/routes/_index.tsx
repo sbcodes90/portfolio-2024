30bcf086601beba0cb0f51c8c0681e79162378c4
sbcodes90 <sydneycburrell29@gmail.com>
2024-01-11 17:37:03 -0500

    netlify remix app

---
+++ my-remix-app/app/routes/_index.tsx
@@ -0,0 +1,25 @@
+export function headers({
+  loaderHeaders,
+  parentHeaders,
+}: {
+  loaderHeaders: Headers;
+  parentHeaders: Headers;
+}) {
+  console.log(
+    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
+  );
+  return {
+    // This is an example of how to set caching headers for a route
+    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
+    "Cache-Control": "public, max-age=60, s-maxage=60",
+  };
+}
+
+export default function Index() {
+  return (
+    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+   
+    </main>
+  );
+}
