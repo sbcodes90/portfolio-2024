386bb5fde44b84faf062f6f7001cfd1b9713c0b3
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-23 22:52:28 -0400

    css updates

--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -14,10 +14,10 @@ function Index() {
     <>
       <div className="w-full">
         <img src={mbanner} alt="banner" className=" h-full w-full" />
-        <section className=" bg-[#e6d06b] grid grid-cols-1 pt-14 lg:px-[100px] lg:py-[75px] lg:grid-cols-2  place-items-center">
+        <section className=" bg-[#d3b00c] grid grid-cols-1 pt-14 lg:px-[100px] lg:py-[75px] lg:grid-cols-2  place-items-center">
           <img src={avi} alt="me" />
 
-          <p className="text-md text-black font-medium mt-[50px] px-[30px] lg:mt-[20px] text-center">
+          <p className="text-sm text-black font-medium mt-[50px] px-[30px] lg:mt-[20px] text-center">
             Hey there, I'm Sydney Burrell, and I've worn multiple hats in the
             tech and design field. I started out as a Graphic Designer,
             transitioned to UX Design, and then dabbled in Email Design,
@@ -32,10 +32,10 @@ function Index() {
               "A day without laughter is wasted" by Charlie Chapman.
             </strong>
             <div className="flex gap-[20px] mt-[20px] pt-[50px] pb-[50px] place-content-center">
-              <button className="bg-transparent  hover:bg-black hover:text-white border border-black text-black font-semibold py-2 px-10 rounded-2xl">
+              <button className="bg-transparent  hover:bg-black hover:text-white border-black text-black border-2 font-semibold py-2 px-10 rounded-2xl">
                 Contact
               </button>
-              <button className="bg-transparent hover:bg-black hover:text-white border border-black text-black font-semibold py-2 px-10   rounded-2xl">
+              <button className="bg-transparent hover:bg-black hover:text-white border-2 border-black text-black font-semibold py-2 px-10   rounded-2xl">
                 See Work
               </button>
             </div>
@@ -43,21 +43,23 @@ function Index() {
         </section>
    
 
-        <section className="pt-[100px]  bg-[#e3bb05] pb-[100px]">
-          <p className="text-2xl font-black text-center">Personal Projects</p>
-          <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-[#e3bb05]">
-            <img src={foodieApp} className="w-[500px]" alt="placeholder" />
-            <div>
-              <p className="font-black text-center">Meal App</p>
-              <p className="px-[50px] py-[25px] font-medium lg:px-[100px]">
+        <section className=" bg-cyan pb-[100px] px-20 pt-10">
+          <p className="text-2xl font-black text-center pb-10 text-black">Personal Projects</p>
+          {/* <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-gradient-to-r from-yellow-300 to-yellow-400 rounded-3xl py-10"> */}
+            <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-[#ffffff] rounded-3xl py-10 shadow-2xl"> 
+
+            <img src={foodieApp} className="pb-5" alt="placeholder" />
+            <div className="bg-white rounded-3xl shadow-2xl lg:mr-20 mx-10  border-black border-1">
+              <p className="font-black text-center mt-5 text-black">Meal App</p>
+              <p className=" font-normal text-sm px-5 py-5 text-center lg:px-[50px] text-black">
                 As a self proclaimed foodie, sometimes at home I want to try to
                 random recipes, so I decided to make a application that browses
                 for you. The Meal API app utilizes a free public API (insert
                 here)
               </p>
 
-              <div className="flex gap-[20px] mt-[20px] pb-[50px] place-content-center">
-                <button className="bg-transparent text-black font-semibold py-2 px-10 border border-black  hover:bg-black hover:text-white  rounded">
+              <div className="flex flex-col justify-center items-center lg:flex-row lg:gap-[20px] mt-[20px] pb-[50px] lg:place-content-center">
+                <button className="bg-transparent text-black font-semibold py-2 px-[45px] mb-2 border border-black  hover:bg-black hover:text-white  rounded">
                   <Link
                     to="https://65ab32bd70a8274031649eb1--mealappbysb.netlify.app/"
                     target="blank"
@@ -66,7 +68,7 @@ function Index() {
                   </Link>
                 </button>
 
-                <button className="bg-transparent text-black font-semibold py-2 px-10 border border-black hover:bg-black hover:text-white  rounded">
+                <button className="bg-transparent text-black font-semibold py-2 px-[50px] border border-black hover:bg-black hover:text-white  rounded">
                   Github
                 </button>
               </div>
--- my-remix-app/tailwind.config.ts
+++ my-remix-app/tailwind.config.ts
@@ -4,6 +4,11 @@ export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}', "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+      colors: { 
+        'cyan': '#d3b00c',
+          
+        // ...
+    },
   },
   plugins: [
     require('flowbite/plugin')
