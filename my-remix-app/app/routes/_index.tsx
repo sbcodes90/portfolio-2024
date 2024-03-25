6bdd8135ac9836c0c6dbf4ecfcca70142d81ec46
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-25 13:10:29 -0400

    css updates

--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -4,20 +4,21 @@ import foodieApp from "../images/foodie-mobile.png";
 import variantPrint from "../images/yellow-variant-background.png";
 import { Link } from "@remix-run/react";
 import TechStack from "~/components/TechStack";
-import authappic from "../images/authapppic.png"
-
+import authappic from "../images/authapp.png"
+import personalProjectHeader from "../images/pp-header.png";
 
 
 
 function Index() {
+  
   return (
     <>
       <div className="w-full">
         <img src={mbanner} alt="banner" className=" h-full w-full" />
-        <section className=" bg-[#d3b00c] grid grid-cols-1 pt-14 lg:px-[100px] lg:py-[75px] lg:grid-cols-2  place-items-center">
+        <section className=" bg-white grid grid-cols-1 pt-14 lg:px-[100px] lg:py-[75px] lg:grid-cols-2  place-items-center">
           <img src={avi} alt="me" />
 
-          <p className="text-sm text-black font-medium mt-[50px] px-[30px] lg:mt-[20px] text-center">
+          <p className="text-sm text-black font-medium mt-[50px] lg:pt-10 px-[50px] lg:px-[30px] lg:mt-[20px]  justify-normal">
             Hey there, I'm Sydney Burrell, and I've worn multiple hats in the
             tech and design field. I started out as a Graphic Designer,
             transitioned to UX Design, and then dabbled in Email Design,
@@ -28,6 +29,7 @@ function Index() {
             Philadelphia, PA, and I love to explore new restaurants and attend
             comedy shows around the city. Laughter is my medicine, and I truly
             believe in the quote,{" "}
+            <br></br>
             <strong>
               "A day without laughter is wasted" by Charlie Chapman.
             </strong>
@@ -42,14 +44,47 @@ function Index() {
           </p>
         </section>
    
+        <section className=" lg:px-20 pt-10">
+          <div className="flex justify-center items-center">
+        <img src={personalProjectHeader} alt="Personal-Projects-Header" />
+        </div>
+            <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 rounded-3xl py-10"> 
+           
+            <img src={foodieApp} className="pb-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 rotate-45..." alt="placeholder" />
 
-        <section className=" bg-cyan pb-[100px] px-20 pt-10">
-          <p className="text-2xl font-black text-center pb-10 text-black">Personal Projects</p>
+            <div className="rounded-3xl shadow-2xl lg:mr-20 mx-10 bg-[#ffffff] border-black border-2">
+              <p className="font-black text-center mt-5 text-black">Meal App</p>
+              <p className=" font-normal text-sm px-5 py-5 text-center lg:px-[50px] text-black">
+                As a self proclaimed foodie, sometimes at home I want to try to
+                random recipes, so I decided to make a application that browses
+                for you. The Meal API app utilizes a free public API (insert
+                here)
+              </p>
+
+              <div className="flex flex-col justify-center items-center lg:flex-row lg:gap-[20px] mt-[20px] pb-[50px] lg:place-content-center">
+                <button className="bg-black text-white font-semibold py-2 px-[45px] mb-2 border border-black  hover:bg-black hover:text-white  rounded">
+                  <Link
+                    to="https://65ab32bd70a8274031649eb1--mealappbysb.netlify.app/"
+                    target="blank"
+                  >
+                    Preview
+                  </Link>
+                </button>
+
+                <button className="bg-black text-white font-semibold py-2 px-[50px] border border-black hover:bg-black hover:text-white  rounded">
+                  Github
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className=" bg-cyan px-20">
           {/* <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-gradient-to-r from-yellow-300 to-yellow-400 rounded-3xl py-10"> */}
-            <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-[#ffffff] rounded-3xl py-10 shadow-2xl"> 
+         {/*    <div className="grid grid-cols-1 place-items-center lg:grid-cols-2  rounded-3xl py-10 shadow-2xl border-white border-2"> 
 
             <img src={foodieApp} className="pb-5" alt="placeholder" />
-            <div className="bg-white rounded-3xl shadow-2xl lg:mr-20 mx-10  border-black border-1">
+            <div className="bg-white rounded-3xl shadow-2xl lg:mr-20 mx-10  border-cyan border-2">
               <p className="font-black text-center mt-5 text-black">Meal App</p>
               <p className=" font-normal text-sm px-5 py-5 text-center lg:px-[50px] text-black">
                 As a self proclaimed foodie, sometimes at home I want to try to
@@ -73,7 +108,7 @@ function Index() {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </section>
         <section className="lg:pt-[50px] bg-[#e6d06b] pb-[100px] px-20">
           <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 pt-[100px]">
@@ -87,35 +122,7 @@ function Index() {
             </div>
           </div>
         </section>
-        <section className="pt-[100px]  bg-[#e3bb05]">
-          <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 bg-[#e3bb05]">
-            <img src={foodieApp} className="w-[500px]" alt="placeholder" />
-            <div>
-              <p className="font-black text-center">Friends App</p>
-              <p className="px-[50px] py-[25px] font-medium lg:px-[100px]">
-                As a self proclaimed foodie, sometimes at home I want to try to
-                random recipes, so I decided to make a application that browses
-                for you. The Meal API app utilizes a free public API (insert
-                here)
-              </p>
-
-              <div className="flex gap-[20px] mt-[20px] pb-[50px] place-content-center">
-                <button className="bg-transparent text-black font-semibold py-2 px-10 border border-black  hover:bg-black hover:text-white  rounded">
-                  <Link
-                    to="https://65ab32bd70a8274031649eb1--mealappbysb.netlify.app/"
-                    target="blank"
-                  >
-                    Preview
-                  </Link>
-                </button>
-
-                <button className="bg-transparent text-black font-semibold py-2 px-10 border border-black hover:bg-black hover:text-white  rounded">
-                  Github
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
+    
         <section className="lg:pt-[50px] bg-[#e6d06b] pb-[100px]">
           <div className="grid grid-cols-1 place-items-center lg:grid-cols-2 pt-[100px]">
             <p>Swift UI App</p>
